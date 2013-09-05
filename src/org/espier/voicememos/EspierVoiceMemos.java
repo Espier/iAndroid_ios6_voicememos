0c881991773baeb3f45f53cd8a97c6320659290c
xueshuming <xueshuming@minigui.org>
2013-09-05 17:56:08 +0800

    init ios6 voicememos project

---
+++ src/org/espier/voicememos/EspierVoiceMemos.java
@@ -0,0 +1,14 @@
+package org.espier.voicememos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EspierVoiceMemos extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
